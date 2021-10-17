f3fb0292e865eb59f8fc24b2e5beae2f4bc104e6
Ganesh <ganeshkadam771@gmail.com>
2021-10-17 18:28:09 +0530

    Forth Commit Added logic for sum with all test cases

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,26 +3,50 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-		    	if(input.equals("")){
-					return 0;
+	    	if(input.equals("")){
+				return 0;
+			}
+			else{
+				String delimiter = ",";
+				if(input.matches("//(.*)\n(.*)")){
+					delimiter = Character.toString(input.charAt(2));
+					input = input.substring(4);
 				}
-				else{
-					String delimiter = ",";
-					if(input.matches("//(.*)\n(.*)")){
-						delimiter = Character.toString(input.charAt(2));
-						input = input.substring(4);
-					}
-					
-					String numList[] = splitNumbers(input, delimiter + "|\n");
-					return sum(numList);
+				
+				String numList[] = splitNumbers(input, delimiter + "|\n");
+				return sum(numList);
 		    }
     }
-		    	private static String[] splitNumbers(String numbers, String divider){
-		    	    return numbers.split(divider);
-		    	}
-		    	
-		    	private static int sum(String[] numbers) {
-		    		return 0;
-		    	}
+    
+		    private static int toInt(String number){
+				return Integer.parseInt(number);
+			}
+
+	    	private static String[] splitNumbers(String numbers, String divider){
+	    	    return numbers.split(divider);
+	    	}
+	    	
+	    	private static int sum(String[] numbers) {
+	    		int total=0;
+	    		String negString="";
+	    		
+	    		for(String number : numbers)
+	    		{
+	    			if(toInt(number)<0) {
+	    				if(negString.equals(""))
+	    					negString=number;
+	    				else
+	    					negString +=(","+number);
+	    			}
+	    			if(toInt(number)<1000)
+	    				total += toInt(number);
+	    		}
+	    		
+	    		if(!negString.equals("")) {
+	    			throw new IllegalArgumentException("Negative Numbers not allowed: "+negString);
+	    		}
+	    		
+	    		return total;
+	    	}
 
 }
