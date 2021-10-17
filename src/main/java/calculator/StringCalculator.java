f09a96c3495ab132ae4b0f99da69b2ba737d9705
Ganesh <ganeshkadam771@gmail.com>
2021-10-17 18:10:45 +0530

    Third Commit First Test case runs properly

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,26 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+		    	if(input.equals("")){
+					return 0;
+				}
+				else{
+					String delimiter = ",";
+					if(input.matches("//(.*)\n(.*)")){
+						delimiter = Character.toString(input.charAt(2));
+						input = input.substring(4);
+					}
+					
+					String numList[] = splitNumbers(input, delimiter + "|\n");
+					return sum(numList);
+		    }
     }
+		    	private static String[] splitNumbers(String numbers, String divider){
+		    	    return numbers.split(divider);
+		    	}
+		    	
+		    	private static int sum(String[] numbers) {
+		    		return 0;
+		    	}
 
 }
