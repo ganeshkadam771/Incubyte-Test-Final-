c910b1924b97ec25a4af449baa18e8925ea29542
Ganesh <ganeshkadam771@gmail.com>
2021-10-17 19:07:05 +0530

    Eighth Commit 4th and 5th test case runs succesfully

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -28,5 +28,16 @@ class StringCalculatorShould {
     	
     }
     
+   @Test
+   public void testThreeNumbers()
+   {
+	   assertEquals(6, StringCalculator.add("1,2,3"));
+   }
+   
+   @Test
+   public void testNewLine() {
+	   assertEquals(6,StringCalculator.add("1\n2,3"));
+   }
+   
    
 }
